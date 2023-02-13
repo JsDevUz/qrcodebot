0bfc50855a5c592a5b452c54ae3c9c0cd0d43450
proger <shakhislom.org@gmail.com>
2023-02-13 10:07:04 +0500

    last

--- server.js
+++ server.js
@@ -1,51 +1,77 @@
-const express = require('express');
-const app = express();
+
 const fs = require('fs')
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-const TeleBot = require('telebot');
-const bot = new TeleBot('5780609524:AAHRBdUG9rvbO3kAjqO0VQ5RnAQ-nlglmHA');
-const QRCode = require('qrcode')
-
-
-bot.on('/start', (msg) => {
-    let newtext = `Salom istalgan matinni kiriting va bot sizga qrcode tayyorlab beradi.`;
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('5780609524:AAHRBdUG9rvbO3kAjqO0VQ5RnAQ-nlglmHA');
+const QRCode = require('qrcode');
+const { unlink } = require('fs/promises');
+const { default: axios } = require('axios');
+const checkUserAvialable = (user) => {
     fs.readFile(`./users.txt`, 'utf8', (err, data) => {
-        if (!(data.indexOf(msg.from.id) + 1) || data.length == 0) {
-            fs.writeFile('./users.txt', `${data}\n${msg.from.id}`, () => { });
+        if (!(data.indexOf(user) + 1) || data.length == 0) {
+            fs.writeFile('./users.txt', `${data}\n${user}`, () => { });
         }
     });
-    msg.reply.text(newtext)
+}
+
+
+bot.start(async (ctx) => {
+    checkUserAvialable(ctx.chat.id)
+
+    let newtext = `Hi send me text or qrcode!`;
+    fs.readFile(`./users.txt`, 'utf8', (err, data) => {
+        if (!(data.indexOf(ctx.chat.id) + 1) || data.length == 0) {
+            fs.writeFile('./users.txt', `${data}\n${ctx.chat.id}`, () => { });
+        }
+    });
+    ctx.reply(newtext)
 });
 
 
-bot.on('text', (msg) => {
-    if (msg.text == '/start') return;
-
-    QRCode.toBuffer(msg.text, function (err, code) {
+bot.on('text', async (ctx) => {
+    if (ctx.message.text == '/start') return;
+    // console.log(ctx.message.text);
+    let name = `${ctx.message.text}.jpeg`;
+    QRCode.toDataURL(ctx.message.text, async function (err, code) {
         if (err) return console.log("error occurred");
-        bot.sendPhoto(msg.from.id, code, { caption: 'created by: @TgQrCodeBot', fileName: 'progerboy.jpeg' })
+        // bot.telegram.sendMessage(ctx.chat.id, code, { parse_mode: 'markdown' })
+        var base64Data = code.replace(/^data:image\/png;base64,/, "");
+        await fs.writeFile(name, base64Data, 'base64', function (err) {
+            console.log(err, 'iii');
+        });
+
+        bot.telegram.sendPhoto(ctx.chat.id, { source: `${name}`, caption: 'created by: @TgQrCodeBot', fileName: 'progerboy.jpeg' }).then(() => {
+            unlink(`./${name}`).catch(e => {
+                console.log('e');
+            })
+
+        }
+
+        )
     })
 });
 
-bot.start();
+bot.on('photo', async (ctx) => {
 
-process.on('unhandledRejection', ex => {
-    console.log(ex, 'uhr');
-    process.exit(1)
-
-})
-process.on('uncaughtException', ex => {
-    console.log(ex, 'unce');
-    process.exit(1)
-})
-app.use(function (err, req, res, next) {
-    console.log(err, 'un');
-    res.status(500).send({
-        message: 'SERVER ERROR', type: 'global'
+    const imageData = await bot.telegram.getFile(ctx.message.photo[ctx.message.photo.length - 1].file_id)
+    bot.telegram.sendMessage(ctx.from.id, 'Scanning...', { reply_to_message_id: ctx.message.message_id })
+    axios({
+        url: `https://api.qrserver.com/v1/read-qr-code/?fileurl=https://api.telegram.org/file/bot5780609524:AAHRBdUG9rvbO3kAjqO0VQ5RnAQ-nlglmHA/${imageData.file_path}`,
+        method: 'GET'
     })
+        .then(async (response) => {
+            if (response.data[0].symbol[0].error === null) {
+                await ctx.reply(`Result: ${response.data[0].symbol[0].data}`)
+            } else {
+                await ctx.reply('No data found on this picture.')
+            }
+
+            ctx.reply('You can send me other pictures')
+        })
+        .catch((err) => {
+            ctx.reply('No data found on this picture.')
+        })
 })
-app.listen(3009, console.log('gooo'))
+bot.launch();
+
+
+
